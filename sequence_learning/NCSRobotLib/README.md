26f9e5a46fb4949940308fefe5f57597857db106
ysandamirskaya <sandayci@rub.de>
2017-03-30 14:46:25 +0200

    deleted extra files

--- sequence_learning/NCSRobotLib/README.md
+++
@@ -1,3 +0,0 @@
-# NCSRobotLib
-
-Library to interface the MadBot, PushBot and Kepera at INI.
--- sequence_learning/NCSRobotLib/src/ROLLSListener_Log.cpp
+++
@@ -1,9 +0,0 @@
-#include <ROLLSListener_Log.h>
-
-void ROLLSListener_Log::receivedNewROLLSEvent(unsigned int neuron) {
-    // time since listening started, in ms
-    pt::time_duration duration = pt::microsec_clock::local_time() - start;
-    timestamp = duration.total_milliseconds();
-
-    fprintf(log, "%ld,%u\n", timestamp, neuron);
-}
