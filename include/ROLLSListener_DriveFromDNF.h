60d5527c89e7a25dfba62080c56d03792f0ecf49
ysandamirskaya <sandayci@rub.de>
2017-04-03 17:14:15 +0200

    NOW added a new ROLLListener_DriveFromDNF

---
+++ include/ROLLSListener_DriveFromDNF.h
@@ -0,0 +1,63 @@
+#ifndef __ROLLS_LISTENER_DRIVE_FROM_DNF_H__
+#define __ROLLS_LISTENER_DRIVE_FROM_DNF_H__
+
+#include <ROLLSListener.h>
+#include <ROLLSDevice.h>  // NeuronGroup
+#include <PushBot.h>
+#include <thread>
+#include <mutex>
+#include <functional>  // bind
+
+class ROLLSListener_DriveFromDNF : public ROLLSListener 
+{
+public:
+    ROLLSListener_DriveFromDNF(PushBot *robot,NeuronGroup dnf)
+    : pushbot(robot), DNF(dnf), motor_thread(std::bind(&ROLLSListener_DriveFromDNF::loop, this)) 
+    {
+	phi_curr = 2;
+	phi_tar = 0;
+	speed_factor = 0;
+	turn_factor = 1;
+    };
+
+    virtual ~ROLLSListener_DriveFromDNF() {_stop = true; motor_thread.join();}
+
+    void receivedNewROLLSEvent(unsigned int neuron);
+
+    void goNoGo(bool stop);
+
+    void setTurnFactor(double factor);
+    
+private:
+    // Pushbot
+    PushBot *pushbot;
+    void loop();
+
+    // Neuron Populations
+    NeuronGroup DNF;
+
+    //current heading direction of the robot
+    int phi_curr;
+
+    //target heading direction, set by activity peak in the DNF
+    int phi_tar;
+
+    //turning command towards the target heading direction
+    int speed;
+
+    // counter and states for spikes
+    int turn_vel, forward_vel;
+
+    // Scaling for speed and turning
+    double speed_factor;
+    double turn_factor;
+    double time_factor;
+
+    // variables for motor thread
+    std::thread motor_thread;
+    std::mutex counter_mutex;
+    bool _stop = true;
+
+};
+
+#endif
---
+++ src/ROLLSListener_DriveFromDNF.cpp
@@ -0,0 +1,48 @@
+#include <ROLLSListener_DriveFromDNF.h>
+#include <chrono>  // milliseconds
+
+// Future improvement: Let this be specified in constructor
+const int INTERVAL = 500;  // time in milliseconds to check firing counts
+//const double WEIGHT_OLD = 0.6;  // Factor for exponential window
+//const double WEIGHT_NEW = 0.4;  // Factor for exponential window
+
+
+
+void ROLLSListener_DriveFromDNF::receivedNewROLLSEvent(unsigned int event) 
+{
+    counter_mutex.lock();
+    if (DNF.isOrigin(event)) 
+	    phi_tar = event - DNF.start - DNF.size() / 2; 
+    counter_mutex.unlock();
+}
+
+void ROLLSListener_DriveFromDNF::goNoGo(bool stop)
+{
+	_stop = stop;
+}
+
+void ROLLSListener_DriveFromDNF::setTurnFactor(double factor)
+{
+	turn_factor = factor;
+}
+
+void ROLLSListener_DriveFromDNF::loop() 
+{
+    // Every INTERVAL time, update motors, then sleep for remaining time
+    // Note: Execution takes less than a millisecond, so we don't time anything
+    while (!_stop) 
+    {
+        counter_mutex.lock();
+	if ( abs(phi_tar - phi_curr) > 0.5 )
+	{
+		time_factor = 1000 / INTERVAL;
+		turn_vel = turn_factor * time_factor * (phi_tar-phi_curr);
+		forward_vel = speed * speed_factor * time_factor;
+	        pushbot->move(forward_vel, turn_vel);
+		std::this_thread::sleep_for(std::chrono::milliseconds(INTERVAL));
+	        pushbot->move(0, 0);
+		phi_curr = phi_curr + turn_vel * INTERVAL;	
+	}
+        counter_mutex.unlock();
+    }
+}
